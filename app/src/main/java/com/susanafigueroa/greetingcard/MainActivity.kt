0168704dae0475691038eb4b412d0fc80d6ab002
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-28 17:39:56 +0200

    add padding

--- app/src/main/java/com/susanafigueroa/greetingcard/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/greetingcard/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.susanafigueroa.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Yellow) {
         Text(
             text = "Hi, my name is $name!",
-            modifier = modifier
+            modifier = Modifier.padding(24.dp)
         )
     }
 }
