003f2ec795a2c330d9de3f5252a69e77acb7513e
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-28 17:35:01 +0200

    optimize imports

--- app/src/main/java/com/susanafigueroa/greetingcard/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/greetingcard/MainActivity.kt
@@ -7,14 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.susanafigueroa.greetingcard.ui.theme.GreetingCardTheme
-
-import androidx.compose.material3.Surface
-import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
